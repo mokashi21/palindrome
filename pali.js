38758fc2605f9c814653b5f1fefbc373f374fa2f
mokashi21 <101655816+mokashi21@users.noreply.github.com>
2022-07-23 13:36:54 +0530

    Add files via upload

---
+++ pali.js
@@ -0,0 +1,23 @@
+
+// Create a program to print all the palindromes under 50.
+//  using  if else we hvae created a palindrome,
+// 
+for (let i=0;i<=50;i++) {
+    // by aassumeing a -= parseInt we declare that i should be divide by 10 
+// This method is used to get the primitive data type of a certain String
+// The main purpose of using the parseInt function is to extract a number from a string. This turns the returned value to an actual number. In the example above, 3 is a string and not an actual number.
+    // 
+    let a= parseInt(i/10);
+    if(i<10) {
+        console.log(i, "");
+    }
+    else if (i/a ==11) {
+        console.log(i, "");
+    }
+}
+
+// --------------------------------end here-------------------------------
+
+
+
+
